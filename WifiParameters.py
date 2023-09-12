9c3b8550aee98ebea5382dd9e6aff2063df60404
Vineeth Kartha <vineeth.kartha@gmail.com>
2023-09-12 21:51:44 +0530

    config file where the user needs to enter the details for wifi setup

---
+++ WifiParameters.py
@@ -0,0 +1,8 @@
+WifiParameters = {
+    'ssid': 'Your SSID',
+    'password': 'your password for the wifi network',
+    'ip': 'choose an IP in your network e.g. 192.168.1.10',
+    'subnet': '255.255.255.0',
+    'gateway': 'choose a gatway value. e.g. 192.168.1.1',
+    'dns': '8.8.8.8'
+}
